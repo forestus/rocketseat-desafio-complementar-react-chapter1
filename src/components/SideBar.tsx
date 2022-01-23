acdc4443d4a51c18f8c46fdcb6a1f962f048b9b6
Guilherme Pereira Maciel <guilhermepm164@gmail.com>
2022-01-23 02:32:34 -0300

    refactory: separate the content and sidebar to components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
+import { GenreResponseProps } from '../App';
+import { Button } from './Button';
+interface IProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClickButton,
+}: IProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
